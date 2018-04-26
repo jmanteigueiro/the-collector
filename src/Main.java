8854bea365dd3f4a51fb596be275c4bd19db65b0
André Monteiro <andremonteiro95@gmail.com>
2018-04-26 14:48:15 +0100

    Projeto Inicial

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    /**
+     * Isto é um Javadoc, serve para descrever um método
+     *
+     * @param args isto é a descrição do parâmetro que é recebido
+     * @return isto é para descrever o valor que o método retorna
+     */
+    public static void main(String[] args) {
+        // Esta função deve ficar simplificada ao máximo
+        //
+        // INFO SOBRE OS PACKAGES:
+        // View       - Classes de UI
+        // Model      - Classes de objetos a ser utilizados na aplicação
+        // View Model - Classes de acesso e modificação dos dados
+        // Security   - Classes "helpers" que contêm os métodos de segurança
+        //
+        // Para obter os dados para a UI, deve invocar-se sempre métodos de classes ViewModel!
+        // Instanciar um objeto da classe ViewModel e ir invocando métodos daí
+
+        System.out.println("Hello World!");
+    }
+}
