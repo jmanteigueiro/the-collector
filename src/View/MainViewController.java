3ce4afc864ab8db5be1fa2c42b5b6e4598ffa31c
Unknown <fabio.pereira@ubi.pt>
2018-05-01 01:40:26 +0100

    Add openfile and load data to table view

--- src/View/MainViewController.java
+++ src/View/MainViewController.java
@@ -1,34 +1,41 @@
 package View;
 
-import Data.CredentialsJSON;
-import Model.Config;
 import Model.Credential;
 import Model.CredentialsList;
+import ViewModel.CredentialsViewModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.awt.Desktop;
 import java.io.File;
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 
 
 public class MainViewController
 {
 
     private static Stage stage;
-    private Desktop desktop = Desktop.getDesktop();
-    private CredentialsList credentialList;
-    private Config config;
+    private CredentialsViewModel credentialsViewModel;
+    private CredentialsList credentialsList;
 
     @FXML
     private MenuItem openFile;
 
     @FXML
-    private TableView<?> dataTable;
+    private TableView<Credential> dataTable;
 
     @FXML
     private MenuItem save;
@@ -61,7 +68,29 @@ public class MainViewController
         );
         File file = fileChooser.showOpenDialog(stage);
         if (file != null) {
-            System.out.println(file.getName());
+            String fileData = file.getName();
+            // e para remover o seguinte codigo e adicionar a parte do ze
+            PrivateKey privateKey = null;
+            try{
+                File filePrivateKey = new File("/home/fabio/Documents/MEI_2017/SSS" + "/sk.pem");
+                FileInputStream fis = new FileInputStream("/home/fabio/Documents/MEI_2017/SSS" + "/sk.pem");
+                byte[] encodedPrivateKey = new byte[(int) filePrivateKey.length()];
+                fis.read(encodedPrivateKey);
+                fis.close();
+
+                KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+                PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(encodedPrivateKey);
+                privateKey = keyFactory.generatePrivate(privateKeySpec);
+
+            }
+            catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+                e.printStackTrace();
+            }
+
+            credentialsViewModel = new CredentialsViewModel(privateKey);
+            credentialsList = credentialsViewModel.getCredentialsList();
+
+            fillDataTable(credentialsList);
         }
     }
 
@@ -77,6 +106,19 @@ public class MainViewController
      */
     @FXML
     void onCloseFile(ActionEvent event) {
+
+    }
+
+    private void fillDataTable(CredentialsList credentialsList){
+        dataTable = new TableView<>();
+        ObservableList<Credential> obsListCredentials = FXCollections.observableArrayList(credentialsList);
+        dataTable.setItems(obsListCredentials);
+
+        TableColumn<Credential, String> columnUsername = new TableColumn<Credential, String>("Username");
+        columnUsername.setCellValueFactory(new PropertyValueFactory("username"));
+        TableColumn<Credential, char[]> columnPass = new TableColumn<Credential, char[]>("Password");
+        columnPass.setCellValueFactory(new PropertyValueFactory("password"));
+
 
     }
 
